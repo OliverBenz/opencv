5ec510da250e18e886db81a12e7a474611400134
OliverBenz <oliver.benz@outlook.com>
2019-10-24 17:44:53 +0200

    ADDED: Object Finder

---
+++ color.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/objdetect.hpp>
+
+using namespace cv;
+
+// Functions
+void detectAndDisplay(Mat frame);
+
+int main(int argc, char** argv){
+  namedWindow("standard");
+  // namedWindow("mask");
+  VideoCapture capture;
+
+  capture.open(0);
+
+  if(! capture.isOpened()) return -1;
+  else{
+    for(;;){
+      Mat frame;
+      capture >> frame;
+      if(frame.empty()) break;
+      
+      detectAndDisplay(frame);
+
+      if(waitKey(10) == 27) break;
+    }
+  }
+
+  return 0;
+}
+
+void detectAndDisplay(Mat frame){
+  Mat mask;
+  cvtColor(frame, mask, COLOR_BGR2HSV);
+  inRange(mask, Scalar(20, 100, 100), Scalar(30, 255, 255), mask);
+
+  Mat cover;
+  findNonZero(mask, cover);
+  Rect rect = boundingRect(cover);
+
+  rectangle(frame, rect.tl(), rect.br(), Scalar(255,0,0), 2);
+
+  imshow("standard", frame);
+  // imshow("mask", mask);
+}
